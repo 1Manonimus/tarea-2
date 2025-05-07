4daade93b5a3f7e8b19248182691423ba97ce95c
Elver Galarga <mangoancestral@gmail.com>
2025-05-06 22:55:11 -0600

    Create 6.c

---
+++ 6.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+float s;
+float pro;
+float mul;
+float res;
+
+{
+    float n;
+printf("ingrese su sueldo para saber su aumento de salario;\n");
+scanf("%f",&s);
+
+
+if(s<1000 & s>0)
+
+{
+    pro=s*15;
+    mul=pro/100;
+    res=s+mul;
+    printf("el aumento de suelo es;%.2f\n",res);
+}
+else
+{
+if(s>=1000)
+{
+    pro=s*12;
+    mul=pro/100;
+    res=s+mul;
+    printf("el aumento de suelo es;%.2f\n",res);
+}
+else
+{
+    printf("el suelo no es valido.\n");
+}
+}
+}
+    return 0;
+}
