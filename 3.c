50e587d2c2ffe3b59ed1bdf72a57ebf59272e134
Elver Galarga <mangoancestral@gmail.com>
2025-05-10 20:51:05 -0600

    Update 3.c

--- 3.c
+++ 3.c
@@ -1,3 +1,4 @@
+//En una Casa de Cabio necesitan construir un programa que dado como dato una cantidad expresada en dólares, convierta esa cantidad en pesos. Construir el diagrama de flujo correspondiente. Tomar en cuenta que: 1dolar ↢ 11.96pesos.
 #include <stdio.h>
 
 int main()
