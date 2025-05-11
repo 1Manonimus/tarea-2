2963ea798663804c98a05caa25d25fb5473cba91
Elver Galarga <mangoancestral@gmail.com>
2025-05-10 20:52:29 -0600

    Update 5.c

--- 5.c
+++ 5.c
@@ -1,3 +1,4 @@
+//En un negocio de productos electrodomésticos aplican un descuento del 8% a todos aquellos clientes cuya compra es superior a $2,500. Dado como dato el monto de la compra del cliente, calcule lo que el cliente debe pagar. Hacer el diagrama de flujo correspondiente.
 #include <stdio.h>
 
 int main()
